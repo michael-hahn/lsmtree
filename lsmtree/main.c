9a61f03c81791b4cb9a30710d41bfff0ab2e7b85
Michael Hahn <mike.han@hotmail.com>
2017-02-14 21:14:14 -0500

    Initial Commit

---
+++ lsmtree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lsmtree
+//
+//  Created by Michael Hahn on 2/14/17.
+//  Copyright © 2017 Michael Hahn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
